47088366af3797d96f99bd541dce56ecc499aadc
Sebastian Pabon <83882464+sebpab@users.noreply.github.com>
2021-11-08 13:26:48 -0500

    Reparacion de errores anterior login
    Cambios principales: Eliminacion del boton "Cancelar" y arreglo general de estructura del html que no permitia tener un login bien centrado

--- tiendaslagenerica_front/tiendasgenericas/src/app/app.component.ts
+++ tiendaslagenerica_front/tiendasgenericas/src/app/app.component.ts
@@ -8,31 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tiendasgenericas';
 
+  user_correcto:string="admininicial";
+  pass_correcto:string="admin123456";
 
- 
+  user:string="";
+  pass:string="";
 
-  entrada1:String = "";
-  entrada2:String = "";
-  mensaje:String = "";
+  correcto:string="";
 
+  comparar(){
 
-
-  validacion(){
-
-   
-
-    if ((this.entrada1 === 'admininicial')&&(this.entrada2 === 'admin123456'))  {
-      this.mensaje = "Acceso Autorizado.  Bienvenid@!";
-      
-    } else {
-      this.mensaje = "Usuario o contraseña errados.  Intente de nuevo.";      
+    if(this.user===this.user_correcto){
+      this.correcto="autorizado";
+      if(this.pass===this.pass_correcto){
+        this.correcto="autorizado";
+      }else{
+        this.correcto="noautorizado";
+      }
+    }else{
+      this.correcto="noautorizado";
     }
 
-  
   }
  
-
-  
 
 
  
