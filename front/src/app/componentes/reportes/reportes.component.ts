fb5858d0f2495c58572b55e96319ef046a54f2af
ernie62 <ernie62@users.noreply.github.com>
2021-12-08 17:29:46 -0500

    moduloListaClientes Terminado
    Se añade la función de display para las tablas de clientes y venta x cliente

--- front/src/app/componentes/reportes/reportes.component.ts
+++ front/src/app/componentes/reportes/reportes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+//import { clienteModel } from '../clientes/clientes.model';
+import { reportesModel } from './reportes.model';
+import { reportesService } from './reportes.service';
 
 @Component({
   selector: 'app-reportes',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reportes.component.css']
 })
 export class ReportesComponent implements OnInit {
-
-  constructor() { }
+  formValue_2!: FormGroup;
+  reportesModelObj: reportesModel = new reportesModel();
+  clienteData!: any;
+  displayTable_clientes: any = 0;
+  displayTable_ventasClnts: any = 0;
+  constructor(private formbuilder: FormBuilder, private api: reportesService) 
+  { }
 
   ngOnInit(): void {
+    /* this.formValue_2 = this.formbuilder.group({
+       cedula: [''],
+       nombrecompleto: [''],
+       direccion: [''],
+       telefono: [''],
+       correo: [''],
+     });
+     this.getAllClientes();*/
+     this.displayTable_clientes = 0;
+     this.displayTable_ventasClnts = 0;
+  } 
+
+  getAllClientes() {
+    this.displayTable_ventasClnts = 0;
+    this.api.getCliente().subscribe((res) => {
+      this.clienteData = res;
+    });
+    this.displayTable_clientes = 1;
   }
 
+  getAllVentasClientes() {
+    this.displayTable_clientes = 0;
+   
+    this.displayTable_ventasClnts = 1;
+  }
+
+
+
 }
+
---
+++ front/src/app/componentes/reportes/reportes.model.ts
@@ -0,0 +1,8 @@
+export class reportesModel {
+    cedula: string = '';
+    nombrecompleto: string = '';
+    direccion: string = '';
+    telefono: string = '';
+    correo: string = '';
+  }
+  
