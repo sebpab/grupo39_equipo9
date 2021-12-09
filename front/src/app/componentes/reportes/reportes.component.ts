b80a02ee53f5248bf348a67af87f275362d520e5
Sebastian Pabon <83882464+sebpab@users.noreply.github.com>
2021-12-09 16:41:29 -0500

    Reportes ventas por clientes

--- front/src/app/componentes/reportes/reportes.component.ts
+++ front/src/app/componentes/reportes/reportes.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 //import { clienteModel } from '../clientes/clientes.model';
 import { reportesModel } from './reportes.model';
 import { reportesService } from './reportes.service';
+import { HttpClient } from '@angular/common/http';
+import { clienteModel } from '../clientes/clientes.model';
+import { ventasModel } from '../ventas/ventas.model';
+import { LoginComponent } from 'src/app/login/login.component';
 
 @Component({
   selector: 'app-reportes',
@@ -15,7 +19,28 @@ export class ReportesComponent implements OnInit {
   clienteData!: any;
   displayTable_clientes: any = 0;
   displayTable_ventasClnts: any = 0;
-  constructor(private formbuilder: FormBuilder, private api: reportesService) 
+
+   //variable receptora de documentos
+   res: any;
+   res2: any;
+  
+  
+   //variable contenedora de contenidos
+   contenido: any;
+   contenido2: any;
+
+   //url api get
+   urlapiGET: string = "http://localhost:8080/api/ventas";
+   urlapiGET2: string = "http://localhost:8080/api/clientes";
+
+   totalventasreporte: any;
+
+   totalesventas: number[] = [];
+
+   
+
+
+  constructor(private formbuilder: FormBuilder, private api: reportesService, private objetohttp: HttpClient, private clientehttp: HttpClient) 
   { }
 
   ngOnInit(): void {
@@ -29,6 +54,9 @@ export class ReportesComponent implements OnInit {
      this.getAllClientes();*/
      this.displayTable_clientes = 0;
      this.displayTable_ventasClnts = 0;
+
+     
+  
   } 
 
   getAllClientes() {
@@ -41,11 +69,63 @@ export class ReportesComponent implements OnInit {
 
   getAllVentasClientes() {
     this.displayTable_clientes = 0;
-   
+    this.totalventasreporte = "";
+    this.totalesventas = [];
+  
+    
+
+
+    this.res = this.objetohttp.get(this.urlapiGET);
+    this.res.subscribe((datos: any[]) => {
+      this.contenido = datos;
+      console.log(this.contenido);     
+
+    });
+
+    this.res2 = this.objetohttp.get(this.urlapiGET2);
+    this.res2.subscribe((datos2: any[]) => {
+      this.contenido2 = datos2;
+      console.log(this.contenido2);     
+      
+    });
+
     this.displayTable_ventasClnts = 1;
+    
+
+    for (let i of this.contenido){
+
+      console.log(i.valorventa);
+      
+      this.totalesventas.push(i.valorventa);
+      
+    }
+
+    console.log(this.totalesventas);
+
+    let sum = this.totalesventas.reduce((a, b) => a + b, 0);
+    
+    console.log(sum);
+
+    this.totalventasreporte = sum;
+
+    sum = 0;
+
+    console.log(this.totalventasreporte);
+    
+  }
+
+    
+    
   }
 
 
 
-}
+
+ 
+
+
+
+
+
+
 
